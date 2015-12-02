06472d8530a1563e37a154c193b9b1f301b44899
Vincent Chen <vince78718@gmail.com>
2015-12-02 10:53:14 +0800

    Update content and add zopim.

---
+++ scripts/scripts.js
@@ -0,0 +1,17 @@
+(function($) {
+    "use strict";
+    
+    /***************** Smooth Scrolling ******************/
+
+    $('a[href*=#]:not([href=#])').on("click", function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({scrollTop: target.offset().top}, 750);
+                return false;
+            }
+        }
+    });
+
+})(jQuery);
